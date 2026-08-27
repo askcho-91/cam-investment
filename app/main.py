25d12f37913994ce401e3dc13110bb22eb2e0d9e
israel-09 <israelinene91@gmail.com>
2026-08-27 16:22:06 +0100

    Add CORS middleware for improved API security and flexibility

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.modules.stocks.router.stock_router import stock_router
 from app.modules.news.router.news_router import stock_router as news_router
+# from app.modules.users.router import user_router
+# from app.modules.auth.router import auth_router
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=["*"],  # Adjust this to your frontend's origin in production
+)
+
+# app.include_router(auth_router)
+# app.include_router(user_router)
 app.include_router(stock_router)
 app.include_router(news_router)
+
 
 @app.exception_handler(Exception)
 async def exception_handler(request, exc):
